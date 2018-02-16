dece8a5c0ae869a9fa3016bd66fa06582a388094
David <david@drtech.co>
2018-02-16 18:49:40 +0200

    Fix grammatical error

--- server/mail/text.js
+++ server/mail/text.js
@@ -6,6 +6,6 @@ https://{{domain}}/{{verification}}`;
 
 exports.resetMailText = `A password reset has been requested for your account.
 
-Click in button below to reset your password. (ignore if you didn't requested this).
+Click on the button below to reset your password. (ignore if you didn't requested this).
 
 https://{{domain}}/{{resetpassword}}`;
